d329b908bf20e029d86cb23e89fd868a7db4ed6d
kumarlalait79 <luckyarya0101@gmail.com>
2025-10-01 16:45:17 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,15 @@
-// next.config.js (Change 1: Public variable export)
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'L-G-Technologies';
-const basePathValue = isProd ? `/${repoName}` : '';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: basePathValue,
-    assetPrefix: isProd ? `${basePathValue}/` : '',
-    
-    // 👇 Naya addition: BASE_PATH ko public karna
-    env: {
-        NEXT_PUBLIC_BASE_PATH: basePathValue, 
-    },
-    
-    images: {
-        unoptimized: true, 
-        loader: 'custom', 
-        loaderFile: './my-custom-image-loader.js', 
-    },
+  images: {
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
